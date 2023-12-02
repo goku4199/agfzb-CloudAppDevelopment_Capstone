7d7f851e3062aa6b6be61e2e67925e198d5ff920
goku4199 <anshumaantiwari123@gmail.com>
2023-12-02 06:41:04 -0500

    added index and dealer review front end page

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -95,17 +95,19 @@ def get_dealerships_by_id(request, dealer_id):
     return HttpResponse(dealer_names)
 
 def get_dealerships(request):
+    context = {}
     print("dg")
     if request.method == "GET":
         url = "http://127.0.0.1:3000/dealerships/get"
     
     # Get dealers from the URL
     dealerships = get_dealers_from_cf(url)
-    print(dealerships)
+    #print(dealerships)
+    context["dealers"] = dealerships
     # Concat all dealer's short name
     dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
     # Return a list of dealer short name
-    return HttpResponse(dealer_names)
+    return render(request, "djangoapp/index.html", context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
@@ -113,6 +115,7 @@ def get_dealerships(request):
 # ...
 
 def get_dealer_details(request,dealer_id):
+    context = {}
     if request.method == "GET":
         url = "http://127.0.0.1:5000/api/get_reviews"
     
@@ -121,7 +124,10 @@ def get_dealer_details(request,dealer_id):
     # Concat all dealer's short name
     #dealer_name = ' '.join([dealer.short_name for dealer in dealerships])
     # Return a list of dealer short name
-    return HttpResponse(reviews)
+    context['dealer_reviews'] = reviews
+    context['dealer_id'] = dealer_id
+    print(reviews)    
+    return render(request, "djangoapp/dealer_details.html", context)
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
